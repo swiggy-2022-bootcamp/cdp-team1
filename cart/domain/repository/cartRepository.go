1a44e6557d14370adf30fb3b8c1fab02a82ddb9f
Aryann Dhir <aryanndhir.1@gmail.com>
2022-04-11 14:39:08 +0530

    service | repository update

--- cart/domain/repository/cartRepository.go
+++ cart/domain/repository/cartRepository.go
@@ -10,6 +10,7 @@ import (
 type CartRepositoryDB interface {
 	Create(db.Cart) (*db.Cart, *error.AppError)
 	Read(id string) (*db.Cart, *error.AppError)
+	ReadAll() (*[]db.Cart, *error.AppError)
 	Update(db.Cart) (*db.Cart, *error.AppError)
 	Delete(db.Cart) (*db.Cart, *error.AppError)
 	DeleteAll() *error.AppError
@@ -57,4 +58,65 @@ func (cdb CartRepository) Read(id string) (*db.Cart, *error.AppError) {
 	// }
 
 	return &db.Cart{}, nil
+}
+
+func (cdb CartRepository) ReadAll() (*[]db.Cart, *error.AppError) {
+
+	// ctx, cxl := context.WithTimeout(context.Background(), 10*time.Second)
+	// defer cxl()
+
+	// orderCollection := Collection(cdb.dbClient, "cart")
+	// order := db.Cart{}
+	// err := orderCollection.FindOne(ctx, bson.M{"_id": id}).Decode(&order)
+
+	// if err != nil {
+	// 	return nil, error.NewUnexpectedError("Unexpected error from DB")
+	// }
+
+	return &[]db.Cart{}, nil
+}
+
+func (cdb CartRepository) Update(cart db.Cart) (*db.Cart, *error.AppError) {
+
+	// ctx, cxl := context.WithTimeout(context.Background(), 10*time.Second)
+	// defer cxl()
+
+	// orderCollection := Collection(cdb.dbClient, "cart")
+	// _, err := orderCollection.UpdateOne(ctx, bson.M{"_id": cart.Id}, bson.M{"$set": cart})
+
+	// if err != nil {
+	// 	return nil, error.NewUnexpectedError("Unexpected error from DB")
+	// }
+
+	return &db.Cart{}, nil
+}
+
+func (cdb CartRepository) Delete(cart db.Cart) (*db.Cart, *error.AppError) {
+
+	// ctx, cxl := context.WithTimeout(context.Background(), 10*time.Second)
+	// defer cxl()
+
+	// orderCollection := Collection(cdb.dbClient, "cart")
+	// _, err := orderCollection.DeleteOne(ctx, bson.M{"_id": cart.Id})
+
+	// if err != nil {
+	// 	return nil, error.NewUnexpectedError("Unexpected error from DB")
+	// }
+
+	return &db.Cart{}, nil
+}
+
+func (cdb CartRepository) DeleteAll() *error.AppError {
+
+	// ctx, cxl := context.WithTimeout(context.Background(), 10*time.Second)
+	// defer cxl()
+
+	// orderCollection := Collection(cdb.dbClient, "cart")
+	// _, err := orderCollection.DeleteMany(ctx, bson.M{})
+
+	// if err != nil {
+	// 	return error.NewUnexpectedError("Unexpected error from DB")
+	// }
+
+	return nil
 }
--- cart/domain/service/cartService.go
+++ cart/domain/service/cartService.go
@@ -8,7 +8,10 @@ import (
 
 type CartService interface {
 	AddToCart(db.Cart) (*db.Cart, *error.AppError)
+	GetAllCart() (*[]db.Cart, *error.AppError)
 	UpdateCart(db.Cart) (*db.Cart, *error.AppError)
+	DeleteCartById(string) (*db.Cart, *error.AppError)
+	DeleteAllCart() *error.AppError
 }
 
 type DefaultCartService struct {
@@ -25,7 +28,46 @@ func (csvc DefaultCartService) AddToCart(cart db.Cart) (*db.Cart, *error.AppErro
 	return u, err
 }
 
-func (csvc DefaultCartService) UpdateCart(id string, Cart db.Cart) (*db.Cart, *error.AppError) {
-	u, err := csvc.CartDB.Update(db.Cart{})
+func (csvc DefaultCartService) GetAllCart() (*[]db.Cart, *error.AppError) {
+
+	u, err := csvc.CartDB.ReadAll()
+
+	if err != nil {
+		return nil, err
+	}
+
 	return u, err
+}
+
+func (csvc DefaultCartService) UpdateCart(id string, Cart db.Cart) (*db.Cart, *error.AppError) {
+
+	u, err := csvc.CartDB.Update(db.Cart{})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return u, err
+}
+
+func (csvc DefaultCartService) DeleteCartById(id string) (*db.Cart, *error.AppError) {
+
+	u, err := csvc.CartDB.Delete(db.Cart{})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return u, err
+}
+
+func (csvc DefaultCartService) DeleteAllCart() *error.AppError {
+
+	err := csvc.CartDB.DeleteAll()
+
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
