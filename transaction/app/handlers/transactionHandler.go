a31e27521703f6dc598c7f0b47a8baea3cdbe339
AadityaKhetan <aadityakhetan123@gmail.com>
2022-04-15 23:30:23 +0530

    Added sonarqube configurations, swagger comments and updated dockerfile

--- transaction/app/handlers/transactionHandler.go
+++ transaction/app/handlers/transactionHandler.go
@@ -19,6 +19,19 @@ func NewTransactionHandler(transactionService services.TransactionService) Trans
 	return TransactionHandler{transactionService: transactionService}
 }
 
+// AddTransactionPoints godoc
+// @Summary To add transaction points for a user.
+// @Description This request will add transaction points to a user account.
+// @Tags Transaction
+// @Schemes
+// @Accept json
+// @Produce json
+// @Param req body models.TransactionAmount true "Transaction details"
+// @Param userId path string true "User id"
+// @Success	200  string		Transaction points added successfully
+// @Failure 400  string   	Bad request
+// @Failure 500  string		Internal Server Error
+// @Router /transaction/{userId} [POST]
 func (th TransactionHandler) AddTransactionPoints(c *gin.Context) {
 	userId := c.Param("userId")
 	var transactionAmount models.TransactionAmount
@@ -46,6 +59,18 @@ func (th TransactionHandler) AddTransactionPoints(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{"message": "Transaction points added successfully"})
 }
 
+// GetTransactionPointsByUserID godoc
+// @Summary To get transaction points of a user.
+// @Description This request will get transaction points from a user account.
+// @Tags Transaction
+// @Schemes
+// @Accept json
+// @Produce json
+// @Param userId path string true "User id"
+// @Success	200  int		Transaction points
+// @Failure 404  string   	User not found
+// @Failure 500  string		Internal Server Error
+// @Router /transaction/{userId} [GET]
 func (th TransactionHandler) GetTransactionPointsByUserID(c *gin.Context) {
 	userId := c.Param("userId")
 	points, err := th.transactionService.GetTransactionPointsByUserId(userId)
@@ -57,6 +82,20 @@ func (th TransactionHandler) GetTransactionPointsByUserID(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{"transaction_points": points})
 }
 
+// UseTransactionPoints godoc
+// @Summary To use transaction points for an order.
+// @Description This request will use transaction points present in user account for passed order ID.
+// @Tags Transaction
+// @Schemes
+// @Accept json
+// @Produce json
+// @Param req body models.TransactionAmount true "Transaction details"
+// @Param userId path string true "User id"
+// @Success	200  {object}	models.TransactionAmount
+// @Failure 400  string   	Bad request
+// @Failure 417  string		Expectation failed
+// @Failure 500  string		Internal Server Error
+// @Router /transaction/use-transaction-points/{userId} [POST]
 func (th TransactionHandler) UseTransactionPoints(c *gin.Context) {
 	userId := c.Param("userId")
 	var transactionAmount models.TransactionAmount
@@ -81,5 +120,5 @@ func (th TransactionHandler) UseTransactionPoints(c *gin.Context) {
 		c.JSON(err.Code, gin.H{"message": err.Message})
 		return
 	}
-	c.JSON(http.StatusOK, gin.H{"message": newTransactionAmount})
+	c.JSON(http.StatusOK, newTransactionAmount)
 }
