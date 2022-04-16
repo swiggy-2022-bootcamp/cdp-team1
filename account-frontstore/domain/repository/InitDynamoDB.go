a5f44ab3738fa6ab95c4233b2bbf534fde59c331
rshantharaju <rshantharaju@deloitte.com>
2022-04-16 20:58:11 +0530

    config files - migrated from local machine config to local file config

--- account-frontstore/domain/repository/InitDynamoDB.go
+++ account-frontstore/domain/repository/InitDynamoDB.go
@@ -1,9 +1,12 @@
 package repository
 
 import (
+	"fmt"
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
+	config2 "qwik.in/account-frontstore/config"
 )
 
 var dbInitialized = false
@@ -12,8 +15,8 @@ var svc *dynamodb.DynamoDB
 func GetDynamoDBInstance() *dynamodb.DynamoDB {
 	if !dbInitialized {
 		config := &aws.Config{
-			Region:   aws.String("us-west-2"),
-			Endpoint: aws.String("dynamodb.us-west-2.amazonaws.com"),
+			Region:      aws.String("us-west-2"),
+			Credentials: credentials.NewStaticCredentials(config2.AccessKeyID, config2.SecretAccessKey, ""),
 		}
 
 		sess := session.Must(session.NewSession(config))
@@ -21,5 +24,13 @@ func GetDynamoDBInstance() *dynamodb.DynamoDB {
 		svc = dynamodb.New(sess)
 		dbInitialized = true
 	}
+
+	//ping the database
+	_, err := svc.ListTables(&dynamodb.ListTablesInput{})
+	if err != nil {
+		fmt.Println("Connection to dynamoDB failed.")
+		return nil
+	}
+	fmt.Println("Connected to DB")
 	return svc
 }
