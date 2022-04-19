e1edaaf3ed12f33352411af774895f86b133b311
rshantharaju <rshantharaju@deloitte.com>
2022-04-19 20:04:36 +0530

    refactored swagger

--- account-frontstore/app/handlers/accountController.go
+++ account-frontstore/app/handlers/accountController.go
@@ -15,6 +15,16 @@ func init() {
 	accountService = service.InitAccountService(&repository.AccountRepository{})
 }
 
+// RegisterAccount godoc
+// @Summary Register Customer
+// @Description Lets register customer in front store.
+// @Tags
+// @Schemes
+// @Accept json
+// @Produce json
+// @Param req body model.Account true "New account to add"
+// @Success	200  {object} model.Account
+// @Router /customer [POST]
 func RegisterAccount(c *gin.Context) {
 	newAccount := model.Account{}
 	json.NewDecoder(c.Request.Body).Decode(&newAccount)
@@ -29,6 +39,16 @@ func RegisterAccount(c *gin.Context) {
 	c.JSON(200, *createdAccount)
 }
 
+// GetAccountById godoc
+// @Summary Get account by ID
+// @Description Lets front store get customer account by customer id.
+// @Tags
+// @Schemes
+// @Accept json
+// @Produce json
+// @Param accessorId path string true "accessor id"
+// @Success	200  {object} model.Account
+// @Router /account/{accessorId} [GET]
 func GetAccountById(c *gin.Context) {
 	fetchedAccount, err := accountService.GetAccountById(c.Param("accessorId"))
 
@@ -41,6 +61,17 @@ func GetAccountById(c *gin.Context) {
 	c.JSON(200, *fetchedAccount)
 }
 
+// UpdateAccount godoc
+// @Summary Update Account
+// @Description Lets front store update account by id.
+// @Tags
+// @Schemes
+// @Accept json
+// @Produce json
+// @Param accessorId path string true "accessorId id"
+// @Param req body model.Account true "Updated account"
+// @Success	200  {object} model.Account
+// @Router /account/{accessorId} [PUT]
 func UpdateAccount(c *gin.Context) {
 	account := model.Account{}
 	json.NewDecoder(c.Request.Body).Decode(&account)
--- account-frontstore/app/routes/accountFrontstoreRoutes.go
+++ account-frontstore/app/routes/accountFrontstoreRoutes.go
@@ -2,13 +2,16 @@ package routes
 
 import (
 	"github.com/gin-gonic/gin"
+	ginSwagger "github.com/swaggo/gin-swagger"
+	"github.com/swaggo/gin-swagger/swaggerFiles"
 	"qwik.in/account-frontstore/app/handlers"
+	_ "qwik.in/account-frontstore/docs"
 )
 
 //Router ..
 func Router(router *gin.Engine) {
 	newRouter := router.Group("account-frontstore/api")
-	//newRouter.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+	newRouter.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 	newRouter.GET("/", handlers.HealthCheck)
 	newRouter.POST("/register", handlers.RegisterAccount)
 	newRouter.GET("/account/:accessorId", handlers.GetAccountById)
