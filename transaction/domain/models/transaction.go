df22a720f43d5f6547efd394c44d90c569686f94
aky91 <aakash28399@gmail.com>
2022-04-24 17:43:47 +0530

    Added gitignore to project

--- transaction/domain/models/transaction.go
+++
@@ -1,6 +0,0 @@
-package models
-
-type Transaction struct {
-	UserId            string `json:"user_id" dynamodbav:"user_id" validate:"required"`
-	TransactionPoints int    `json:"transaction_points" dynamodbav:"transaction_points"`
-}
