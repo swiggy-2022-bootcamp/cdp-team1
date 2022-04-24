c53fedfd61f59fcfe0e15a5c93336b20501d1b20
aky91 <aakash28399@gmail.com>
2022-04-24 17:47:16 +0530

    Added gitignore

--- auth/dto/healthResponseDTO.go
+++
@@ -1,5 +0,0 @@
-package dto
-
-type HealthResponseDTO struct {
-	Status string `json:"status"`
-}
--- auth/dto/logoutResponseDTO.go
+++
@@ -1,5 +0,0 @@
-package dto
-
-type LogoutResponseDTO struct {
-	Message string `json:"message,omitempty"`
-}
