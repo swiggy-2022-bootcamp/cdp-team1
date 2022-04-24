04cbf1bedc415dcd6327b6140f82dcd13c4f119e
aky91 <aakash28399@gmail.com>
2022-04-24 12:44:20 +0530

    Fixed gRPC and updated routes to api/products

--- productsFrontStore/app/routes/productRoutes.go
+++ productsFrontStore/app/routes/productRoutes.go
@@ -10,10 +10,12 @@ import (
 )
 
 func InitRoutes(router *gin.Engine, handler handlers.ProductHandler) {
-	router.GET("/", handlers.HealthCheck)
 
-	newRouter := router.Group("products/api")
+	newRouter := router.Group("api/products/")
+
+	newRouter.GET("/health", handlers.HealthCheck)
 	newRouter.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+
 	newRouter.GET("/", handler.GetProducts)
 	newRouter.GET("/:id", handler.GetProductById)
 	newRouter.GET("/category/:id", handler.GetProductByCategory)
