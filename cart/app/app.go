693025f6ef6feee684a29faaaff539095e4cce1f
Aryann Dhir <aryanndhir.1@gmail.com>
2022-04-24 23:16:21 +0530

    grpc setup

--- cart/app/app.go
+++ cart/app/app.go
@@ -7,12 +7,19 @@ import (
 	"cartService/domain/repository"
 	"cartService/domain/service"
 	"cartService/log"
+	"cartService/protos"
 	"context"
 	"io"
+	"net"
 	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+	"time"
 
 	"github.com/aws/aws-sdk-go/service/dynamodb"
 	"github.com/gin-gonic/gin"
+	"google.golang.org/grpc"
 )
 
 var (
@@ -24,6 +31,8 @@ var (
 	ctx                context.Context
 	cartDB             *dynamodb.DynamoDB
 	healthCheckHandler handlers.HealthCheckHandler
+	cartServiceProto   service.CartProtoServer
+	wg                 sync.WaitGroup
 )
 
 func Start() {
@@ -40,12 +49,33 @@ func Start() {
 	cartHandler = handlers.NewCartHandler(cartService)
 	healthCheckHandler = handlers.NewHealthCheckHandler(cartRepository)
 	cartRoutes = routes.NewCartRoutes(cartHandler, healthCheckHandler)
+	cartServiceProto = service.NewCartProtoService(cartRepository)
 
 	file, err := os.OpenFile("server.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err == nil {
 		gin.DefaultWriter = io.MultiWriter(file)
 	}
 
+	// gracefulStop logic to allow go routines to finish
+	var gracefulStop = make(chan os.Signal)
+	signal.Notify(gracefulStop, syscall.SIGTERM)
+	signal.Notify(gracefulStop, syscall.SIGINT)
+	go func() {
+		sig := <-gracefulStop
+		log.Info("caught sig: %+v", sig)
+		log.Info("Wait for 2 second to finish processing")
+		time.Sleep(2 * time.Second)
+		os.Exit(0)
+	}()
+
+	//Starting REST server and GRPC server
+	wg.Add(2)
+	go StartRestServer()
+	go StartGRPCServer()
+	wg.Wait()
+}
+
+func StartRestServer() {
 	//Configuring gin server and router
 	server = gin.New()
 	server.Use(log.UseLogger(log.DefaultLoggerFormatter), gin.Recovery())
@@ -53,10 +83,28 @@ func Start() {
 	cartRoutes.InitRoutes(router)
 
 	//Starting server on port 5000
-	err = server.Run(":5000")
+	err := server.Run(":5000")
 	if err != nil {
 		log.Error(err.Error() + " - Failed to start server")
 	} else {
 		log.Info("Server started successfully.")
 	}
+}
+
+func StartGRPCServer() {
+	defer wg.Done()
+	//Opening PORT 9004 for GRPC server
+	lis, err := net.Listen("tcp", ":5004")
+	if err != nil {
+		log.Error("Failed to listen on port %s with error %v", "9004", err)
+	}
+
+	//Creating and registering the GRPC server
+	grpcServer := grpc.NewServer()
+	protos.RegisterCartServer(grpcServer, cartServiceProto)
+	err = grpcServer.Serve(lis)
+	if err != nil {
+		log.Error("Failed to start the grpc server : %v", err)
+	}
+	log.Info("GRPC server started successfully")
 }
---
+++ cart/domain/service/cartServiceProto.go
@@ -0,0 +1,46 @@
+package service
+
+import (
+	"cartService/domain/repository"
+	"cartService/protos"
+	"context"
+)
+
+var cartRepository repository.CartRepositoryDB
+
+type CartProtoServer struct {
+	protos.UnimplementedCartServer
+}
+
+func NewCartProtoService(pr repository.CartRepositoryDB) CartProtoServer {
+	cartRepository = pr
+	return CartProtoServer{}
+}
+
+func (c CartProtoServer) GetCart(ctx context.Context, req *protos.GetCartRequest) (*protos.GetCartResponse, error) {
+
+	products := make([]*protos.Product, 0)
+	response := &protos.GetCartResponse{
+		CustomerId: req.GetCustomerId(),
+		Products:   products,
+	}
+
+	customer_id := req.GetCustomerId()
+
+	cart, err := cartRepository.Read(customer_id)
+	if err != nil {
+		return nil, err.Error()
+	}
+
+	var productProto *protos.Product
+	for _, product := range cart.Products {
+		productProto = &protos.Product{
+			ProductId: product.ProductId,
+			Quantity:  int32(product.Quantity),
+		}
+		products = append(products, productProto)
+	}
+
+	response.Products = products
+	return response, nil
+}
