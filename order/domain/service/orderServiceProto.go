a0268488e3bc27185a50fad60f63b32f01a04025
Aryann Dhir <aryanndhir.1@gmail.com>
2022-04-24 22:35:41 +0530

    protos

---
+++ order/domain/service/orderServiceProto.go
@@ -0,0 +1,23 @@
+package service
+
+import (
+	"orderService/domain/repository"
+	"orderService/protos"
+)
+
+var orderRepository repository.OrderRepository
+
+type OrderProtoServer struct {
+	protos.UnimplementedOrderServer
+}
+
+func NewOrderProtoService(pr repository.OrderRepository) OrderProtoServer {
+	orderRepository = pr
+	return OrderProtoServer{}
+}
+
+func (o OrderProtoServer) CreateOrder(ctx context.Context, req *protos.CreateOrderRequest) (*protos.CreateOrderResponse, error) {
+
+	protos.GetCart(req.CartId)
+
+}
