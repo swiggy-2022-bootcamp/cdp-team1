9ff30d0ae9ef735ea165ef4304052e274c134424
AKASH YADAV <aakash28399@gmail.com>
2022-04-26 00:52:50 +0530

    Products admin (#8)
    * Initial Commit For products-admin
    
    * Added Entities and Related Tests
    
    * Added Health API
    
    * Standardized Router, Setup Swagger, Config file
    
    * Added POST route to add product
    
    * Added other routes, service & repository layer, dummy DB
    
    * Create, Read, Delete Operations using DynamoDB
    
    * Create, Read, Delete Operations using DynamoDB
    
    * Update using DynamoDB
    
    * Updated Dockerfile
    
    * Unit Tests for Service Package
    
    * Unit Tests for Rest API
    
    * gRPC client change for Get Product Quantity
    
    * refactoring gRPC configuration
    
    * Updated docker and related files
    
    * Refactoring and docker-compose update
    
    * Updated docker-compose file
    
    * fixed gRPC ip and port
    
    * gRPC client
    
    * Fixed gRPC and Updated Swagger Doc
    
    * Using docker image from docker hub
    
    * Added gitignore
    
    * Added gRPC server for Product Price
    
    * Undo changes to parent gitignore
    
    * Added prometheus
    
    * Modified ProductPrice gRPC structure
    
    * Modified ProductPrice gRPC structure
    
    * Added detach flag in startup.sh
    
    * Renamed env file
    
    * Create Products Table If Not Present
    
    * Create Products Table Error Handling
    
    Co-authored-by: Aaditya Khetan <aadityakhetan123@gmail.com>

---
+++ productsAdmin/repository/mockRepository.go
@@ -0,0 +1,44 @@
+package repository
+
+import (
+	"github.com/stretchr/testify/mock"
+	"qwik.in/productsAdmin/entity"
+)
+
+type MockRepository struct {
+	mock.Mock
+}
+
+func (mock *MockRepository) Connect() error {
+	args := mock.Called()
+	return args.Error(0)
+
+}
+
+func (mock *MockRepository) FindOne(productId string) (entity.Product, error) {
+	args := mock.Called()
+	result := args.Get(0)
+	return result.(entity.Product), args.Error(1)
+}
+
+func (mock *MockRepository) FindAll() ([]entity.Product, error) {
+	args := mock.Called()
+	result := args.Get(0)
+	return result.([]entity.Product), args.Error(1)
+}
+
+func (mock *MockRepository) SaveProduct(product entity.Product) error {
+	args := mock.Called()
+	return args.Error(0)
+}
+
+func (mock *MockRepository) DeleteProduct(productId string) error {
+	args := mock.Called()
+	return args.Error(0)
+}
+
+func (mock *MockRepository) FindWithLimit(limit int64) ([]entity.Product, error) {
+	args := mock.Called()
+	result := args.Get(0)
+	return result.([]entity.Product), args.Error(1)
+}
