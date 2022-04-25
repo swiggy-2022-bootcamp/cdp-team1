8e1800d7ce7b6894b36e3ec1f048f94079f21b32
yashm1 <yamantri007@gmail.com>
2022-04-25 17:45:38 +0530

    Rewards docker

--- auth/dto/loginRequestDTO.go
+++
@@ -1,7 +0,0 @@
-package dto
-
-type LoginRequestDTO struct {
-	Username string `json:"username"`
-	Email    string `json:"email"`
-	Password string `json:"password"`
-}
--- transaction/domain/models/transactionDetails.go
+++
@@ -1,7 +0,0 @@
-package models
-
-type TransactionDetails struct {
-	UserId  string `json:"user_id"`
-	OrderId string `json:"order_id" validate:"required"`
-	Amount  int    `json:"amount" validate:"required"`
-}
