e506d790b9c9407f68e419f0760ffa005e5d56fc
Aaditya Khetan <aadityakhetan123@gmail.com>
2022-04-26 15:08:11 +0530

    Updated startup.sh

--- rewards/startup.sh
+++ rewards/startup.sh
@@ -1,5 +1,2 @@
-<<<<<<< HEAD
-docker-compose --env-file .env up --build
-=======
-docker-compose --env-file ./dev.env up --build -d
->>>>>>> 73c19b378103bcd1dfc44d4fc38817c2dca5b3b9
+docker-compose --env-file .env up --build -d
+
