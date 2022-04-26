784944affc25653a0f784ccb647dce8a83fc318e
Ashwin G <47941624+ashwin2125@users.noreply.github.com>
2022-04-26 10:28:37 +0530

    completed shippingaddress service implementation (#18)
    * shipping : added healthcheck + dockerfile + custom logger
    
    Initialized the branch, defined folder structures, added Health Check (/checkout/api/), configured Dockerfile for shipping-address service.
    
    * Implemented : DynamoDB
    
    * folder structure revamp
    
    * shipping | CRUD | BL
    
    * shipping : Added Jenkinsfile | minorfix
    
    * shipping : minor file changes
    
    * shipping : BL - bugfix
    
    * shipping : added defaultAddress feature
    
    * shipping : minor fix
    
    * Update .gitignore
    
    * Update .gitignore
    
    * Shipping :   ShippingCost Fix
    
    * shipping : minor fix
    
    * Update Dockerfile
    
    * shipping : minor fix | refactor go.mod
    
    * shipping : minor fix | gRPC
    
    * shipping : minor-fix | gRPC
    
    * shipping : minor-fix | gRPC Server
    
    * shipping : gRPC - Address fix
    
    * shipping : minor-fix
    
    * Delete Jenkinsfile

---
+++ shipping/app/shippingAddrHandler.go
@@ -0,0 +1,149 @@
+package app
+
+import (
+	"fmt"
+	"qwik.in/shipping/domain/services"
+	"qwik.in/shipping/domain/tools/logger"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+//ShippingHandler ..
+type ShippingHandler struct {
+	ShippingAddrService services.ShippingAddrService
+}
+
+//ShippingAddrDTO ..
+type ShippingAddrDTO struct {
+	UserID            int    `json:"user_id" dynamodb:"user_id"`
+	ShippingAddressId string `json:"shipping_address_id" dynamodb:"shipping_address_id"`
+	FirstName         string `json:"first_name" dynamodbav:"first_name"`
+	LastName          string `json:"last_name" dynamodbav:"last_name"`
+	AddressLine1      string `json:"address_line_1" dynamodbav:"address_line_1" `
+	AddressLine2      string `json:"address_line_2" dynamodbav:"address_line_2"`
+	City              string `json:"city" dynamodbav:"city"`
+	State             string `json:"state" dynamodbav:"state"`
+	Phone             string `json:"phone" dynamodbav:"phone"`
+	Pincode           int    `json:"pincode" dynamodbav:"pincode"`
+	AddressType       string `json:"address_type" dynamodbav:"address_type"`
+	DefaultAddress    bool   `json:"default_address" dynamodbav:"default_address"`
+}
+
+// CreateShippingAddrHandlerFunc ..
+// @Summary      Creates New Shipping Address
+// @Description  Creates a new shipping address for the user.
+// @Tags         ShippingAddress Service
+// @Produce      json
+// @Success      200  {object}  map[string]interface{}
+// @Failure      400  {number} 	http.StatusBadRequest
+// @Router       /newAddress    [post]
+func (sh ShippingHandler) CreateShippingAddrHandlerFunc() gin.HandlerFunc {
+
+	return func(ctx *gin.Context) {
+		var saDto ShippingAddrDTO
+		if err := ctx.BindJSON(&saDto); err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{"message": err.Error()})
+			return
+		}
+
+		res, err := sh.ShippingAddrService.CreateNewShippingAddress(
+			saDto.UserID,
+			saDto.ShippingAddressId,
+			saDto.FirstName,
+			saDto.LastName,
+			saDto.AddressLine1,
+			saDto.AddressLine2,
+			saDto.City,
+			saDto.State,
+			saDto.Phone,
+			saDto.Pincode,
+			saDto.AddressType,
+			saDto.DefaultAddress,
+			0,
+		)
+
+		if err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{"message": err.Error()})
+			fmt.Println(err)
+			return
+		}
+		ctx.JSON(http.StatusAccepted, gin.H{"message": "New Shipping Address Created ✅", "Shipping Address ID": res})
+	}
+}
+
+// GetShippingAddrHandlerFunc ..
+// @Summary      Get Shipping Address By ShippingAddressId
+// @Description  Returns shippingAddress from ShippingAddressId
+// @Tags         ShippingAddress Service
+// @Produce      json
+// @Success      200  {object}  map[string]interface{}
+// @Failure      400  {number} 	http.StatusBadRequest
+// @Router       /getAddress/:id    [get]
+func (sh ShippingHandler) GetShippingAddrHandlerFunc() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		id := ctx.Param("id")
+		fmt.Println(id)
+		res, err := sh.ShippingAddrService.GetShippingAddressById(id)
+
+		if err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{"message": "Record not found"})
+			return
+		}
+		ctx.JSON(http.StatusAccepted, gin.H{"record": res})
+	}
+}
+
+// GetDefaultShippingAddrHandlerFunc ..
+// @Summary      Gets Default Shipping Address
+// @Description  Returns default Shipping Address
+// @Tags         ShippingAddress Service
+// @Produce      json
+// @Success      200  {object}  map[string]interface{}
+// @Failure      400  {number} 	http.StatusBadRequest
+// @Router       /existing/   [get]
+func (sh ShippingHandler) GetDefaultShippingAddrHandlerFunc() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		userId := ctx.Param("id")
+		//fmt.Println(userId)
+		userIdAsInt, _ := strconv.Atoi(userId)
+		res, err := sh.ShippingAddrService.GetDefaultShippingAddr(userIdAsInt)
+		if err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{"message": "Provided Shipping Address does not exist"})
+			return
+		}
+		logger.Info("Default Address Fetched ✅ ", res)
+		ctx.JSON(http.StatusAccepted, res)
+	}
+}
+
+func (sh ShippingHandler) GetAllShippingAddrOfUserHandlerFunc() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		userId := ctx.Param("id")
+		//fmt.Println(userId)
+		userIdAsInt, _ := strconv.Atoi(userId)
+		res, err := sh.ShippingAddrService.GetAllShippingAddressOfUser(userIdAsInt)
+		if err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{"message": "Provided Shipping Address does not exist"})
+			return
+		}
+		logger.Info("Default Address Fetched ✅ ", res)
+		ctx.JSON(http.StatusAccepted, res)
+	}
+}
+
+//
+////GetAllShippingAddrOfUserHandlerFunc ..
+//func (sh ShippingHandler) GetAllShippingAddrOfUserHandlerFunc() gin.HandlerFunc {
+//	return func(ctx *gin.Context) {
+//		userId := ctx.Param("id")
+//		//fmt.Println(userId)
+//		userIdAsInt, _ := strconv.Atoi(userId)
+//		userShippingAddress, _ := sh.ShippingAddrService.GetAllShippingAddressOfUser(userIdAsInt)
+//		data, err := json.Marshal(userShippingAddress)
+//		fmt.Println(string(data))
+//		logger.Error(err)
+//		ctx.JSON(http.StatusOK, userShippingAddress)
+//	}
+//}
---
+++ shipping/domain/services/shippingAddrService.go
@@ -0,0 +1,63 @@
+package services
+
+import (
+	"qwik.in/shipping/domain/models"
+	"qwik.in/shipping/domain/repository"
+	"qwik.in/shipping/domain/tools/errs"
+)
+
+//ShippingAddrService ..
+type ShippingAddrService interface {
+	CreateNewShippingAddress(int, string, string, string, string, string, string, string, string, int, string, bool, int) (string, *errs.AppError)
+	GetShippingAddressById(string) (*repository.ShippingAddress, *errs.AppError)
+	GetDefaultShippingAddr(int) (*repository.ShippingAddress, *errs.AppError)
+	GetAllShippingAddressOfUser(int) (*[]models.ShippingAddrModel, *errs.AppError)
+}
+
+//ShippingAddrServiceImpl ..
+type ShippingAddrServiceImpl struct {
+	shippingAddrRepo repository.ShippingAddrRepo
+}
+
+//ShippingAddressServiceFunc ..
+func ShippingAddressServiceFunc(shippingAddressRepository repository.ShippingAddrRepo) ShippingAddrService {
+	return &ShippingAddrServiceImpl{
+		shippingAddrRepo: shippingAddressRepository,
+	}
+}
+
+//CreateNewShippingAddress ..
+func (s ShippingAddrServiceImpl) CreateNewShippingAddress(userID int, shippingAddressId, firstName, lastName, addressLine1, addressLine2, city, state, phone string, pincode int, addressType string, defaultAddress bool, shippingCost int) (string, *errs.AppError) {
+	shippingAddress := repository.ShippingAddrFunc(userID, shippingAddressId, firstName, lastName, addressLine1, addressLine2, city, state, phone, pincode, addressType, defaultAddress, shippingCost)
+	data, err := s.shippingAddrRepo.CreateNewShippingAddrImpl(*shippingAddress)
+	if err != nil {
+		return "", err
+	}
+	return data, nil
+}
+
+//GetShippingAddressById ..
+func (s ShippingAddrServiceImpl) GetShippingAddressById(shippingAddressId string) (*repository.ShippingAddress, *errs.AppError) {
+	res, err := s.shippingAddrRepo.FindShippingAddressByIdImpl(shippingAddressId)
+	if err != nil {
+		return nil, err
+	}
+	return res, nil
+}
+
+//GetDefaultShippingAddr ..
+func (s ShippingAddrServiceImpl) GetDefaultShippingAddr(userId int) (*repository.ShippingAddress, *errs.AppError) {
+	data, err := s.shippingAddrRepo.FindDefaultShippingAddressImpl(userId)
+	if err != nil {
+		return nil, err
+	}
+	return data, nil
+}
+
+func (s ShippingAddrServiceImpl) GetAllShippingAddressOfUser(userId int) (*[]models.ShippingAddrModel, *errs.AppError) {
+	data, err := s.shippingAddrRepo.FindAllShippingAddressOfUser(userId)
+	if err != nil {
+		return nil, err
+	}
+	return data, nil
+}
